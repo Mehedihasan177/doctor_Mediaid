7186d401db3bd88c1a3f9f412c45fbc6abe2f8ed
skabirgithub <abirshir1996@gmail.com>
2021-12-24 11:42:41 +0600

    tuk tak

--- lib/widget/notification_widget/notification_widget.dart
+++ lib/widget/notification_widget/notification_widget.dart
@@ -1,26 +1,21 @@
-import 'package:care_plus_doctor/model/ui_model/notification_model/notification_model.dart';
 import 'package:care_plus_doctor/responses/doctor/doctor_notification_responses.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-Widget NotificationWidget(Doctornotification notification) =>
-    Padding(
+Widget NotificationWidget(Doctornotification notification) => Padding(
       padding: const EdgeInsets.only(left: 10, right: 10),
-      child:   Card(
+      child: Card(
         child: Padding(
           padding: const EdgeInsets.only(top: 15, bottom: 10, left: 10),
           child: Row(
             children: [
-
-
               Expanded(
                 flex: 1,
                 child: Container(
                   height: 45,
                   width: 45,
                   decoration: BoxDecoration(
-                    borderRadius:
-                    BorderRadius.all(Radius.circular(10)),
+                    borderRadius: const BorderRadius.all(Radius.circular(10)),
                     color: Colors.blue.withOpacity(0.2),
                   ),
                   child: Icon(
@@ -30,10 +25,6 @@ Widget NotificationWidget(Doctornotification notification) =>
                   ),
                 ),
               ),
-
-
-
-
               Expanded(
                 flex: 6,
                 child: Padding(
@@ -44,50 +35,42 @@ Widget NotificationWidget(Doctornotification notification) =>
                     children: [
                       Container(
                           alignment: Alignment.centerLeft,
-                          child: Text(notification.title,
-                            style: TextStyle(
-                                fontSize: 15,
-                                fontWeight: FontWeight.bold
-                            ),
-                          )
+                          child: Text(
+                            notification.title,
+                            style: const TextStyle(
+                                fontSize: 15, fontWeight: FontWeight.bold),
+                          )),
+                      const SizedBox(
+                        height: 10,
                       ),
-
-                      SizedBox(height: 10,),
-
                       Container(
                           alignment: Alignment.centerLeft,
-                          child: Text(notification.body,
-                            style: TextStyle(
+                          child: Text(
+                            notification.body,
+                            style: const TextStyle(
                               fontSize: 9,
                               //fontWeight: FontWeight.bold
                             ),
-                          )
+                          )),
+                      const SizedBox(
+                        height: 10,
                       ),
-
-
-                      SizedBox(height: 10,),
-
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Container(
                               alignment: Alignment.bottomLeft,
-                              child: Text(DateFormat("dd-MMM-yyyy").format(notification.createdAt))
-                          ),
-
-
+                              child: Text(DateFormat("dd-MMM-yyyy")
+                                  .format(notification.createdAt))),
                           const Text(" || "),
-
-
                           Container(
                               alignment: Alignment.bottomLeft,
-                              child: Text(DateFormat.jms().format(DateTime.parse(notification.createdAt.toString())))
-                          ),
-
+                              child: Text(DateFormat.jms().format(
+                                  DateTime.parse(
+                                      notification.createdAt.toString())))),
                         ],
                       ),
-
                     ],
                   ),
                 ),
